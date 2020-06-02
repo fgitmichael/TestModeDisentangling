5b4f82783389b1cc59bf0d347f16a0d1e3d05c23
Michael <michael--steinberger@web.de>
2020-06-02 11:14:19 +0200

    change Encoder network for the state representation to have hidden layers

--- code/network/latent.py
+++ code/network/latent.py
@@ -117,12 +117,15 @@ class Encoder(BaseNetwork):
 
 class EncoderStateRep(BaseNetwork):
 
-    def __init__(self, input_dim, output_dim, leaky_slope=0.2):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 leaky_slope=0.2):
         super(EncoderStateRep, self).__init__()
 
         self.net = create_linear_network(input_dim,
                                          output_dim,
-                                         hidden_units=[])
+                                         hidden_units=[24])
 
     def forward(self, x):
         return self.net(x)
